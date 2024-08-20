ef34703eb47a13bbe5c28ab415017bc4ba1cc8fe
Rylee Texter <ryleetexter@gmail.com>
2024-08-20 12:14:40 -0500

    Initial Commit

---
+++ Lab0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab0
+//
+//  Created by rylee texter on 8/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
